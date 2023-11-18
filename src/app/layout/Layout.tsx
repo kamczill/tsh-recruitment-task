5f87836cfb2a5582d0600c3b26077219c73c2a86
kamczill <kam134@interia.eu>
2023-11-18 19:36:52 +0100

    feat: create navbar and icon components

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import './Layout.css';
 
-import Navbar from 'ui/navbar/Navbar';
+import { Navbar } from 'ui/navbar/Navbar';
 
 export const Layout = () => {
   return (
