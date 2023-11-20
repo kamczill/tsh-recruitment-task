8088fa0585ca641952e3f1aed64561e60c5bb926
kamczill <kam134@interia.eu>
2023-11-20 17:45:09 +0100

    refactor(Home): switch to ProductsContext for data management

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,47 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
+// import { useProducts, useQuery } from 'hooks';
+import { useContext } from 'react';
 
+import { ProductsContext } from 'context/products/productsContext/ProductsContext';
 import { ProductCard } from 'ui/productCard/ProductCard';
-import axiosClient from 'api/axios';
+import { ProductCardProps } from 'ui/productCard/ProductCard.types';
 
 export const Home = () => {
-  const getProducts = async () => {
-    try {
-      const response = await axiosClient('/products?limit=8');
-      return response.data; // Ensure you return the data from the function
-    } catch (err) {
-      throw err; // Rethrow the error so React Query can catch it
-    }
-  };
-
-  const {
-    isLoading,
-    isError,
-    data, // This will hold the products data
-    error,
-  } = useQuery(['products'], getProducts); // Changed the query key to 'products'
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  // const {
-  //   data: usersResponse,
-  //   isFetching: isFetchingUsers,
-  //   isFetched: areUsersFetched,
-  //   hasNextPage: hasMoreUsers,
-  //   fetchNextPage: loadMoreUsers,
-  //   isFetchingNextPage,
-  // } = useUsers();
+  const context = useContext(ProductsContext);
+  // const {isLoading, isSuccess, isError, isFetching} = useProducts();
 
   return (
     <>
       <div className="w-full flex justify-center">
         <div className="flex flex-wrap justify-center gap-6 mt-6 lg:max-w-[1224px]">
-          {data?.items.map((prod) => (
+          {context?.data?.items?.map((prod: ProductCardProps) => (
             <ProductCard
               key={prod.id}
               active={prod.active}
@@ -55,72 +27,6 @@ export const Home = () => {
           ))}
         </div>
       </div>
-
-      {/* <h2>Home</h2>
-      <p>
-        <Translation id="home.helloWorld" />
-        <span style={{ margin: '0 1rem' }}>&#x2190;</span>
-        <span>
-          This text is translated using <strong>Translation</strong> component.
-        </span>
-        <span>Click </span>
-        <button
-          style={{ fontSize: 'inherit' }}
-          onClick={() => setLocale(locale === AppLocale.pl ? AppLocale.en : AppLocale.pl)}
-        >
-          here
-        </button>{' '}
-        to change language.
-      </p>
-      <p>This is a starter project for TSH React application. Click on navigation links above to learn more.</p>
-      <hr />
-      <LocationInfo />
-      <hr />
-      <div style={{ marginBottom: '2rem' }}>
-        <p>User information &#129489;</p>
-        <div style={{ marginBottom: '2rem', display: 'flex', gap: '16px' }}>
-          <button
-            disabled={isAuthenticating || isAuthenticated}
-            onClick={() => login({ password: 'tsh-react-starter', username: 'tsh' })}
-          >
-            {isAuthenticating ? 'Logging in...' : 'Click to login'}
-          </button>
-          <button disabled={!isAuthenticated} onClick={logout}>
-            Click to logout
-          </button>
-        </div>
-        {isAuthenticating && <p>Loading data about you...</p>}
-        {isAuthenticated && (
-          <code style={{ background: '#BADA55', padding: '1rem' }}>{JSON.stringify(user, null, 2)}</code>
-        )}
-      </div>
-      <div>
-        <p>List of users &#129489;</p>
-        <div style={{ marginBottom: '2rem' }}>
-          <ul>
-            {areUsersFetched &&
-              usersResponse?.pages?.map((page, index) => (
-                <Fragment key={index}>
-                  {page.users?.map((user) => (
-                    <li key={user.id}>
-                      <button
-                        onClick={() => {
-                          navigate(AppRoute.user, { params: { id: user.id } });
-                        }}
-                      >
-                        User {user.id}
-                      </button>
-                    </li>
-                  ))}
-                </Fragment>
-              ))}
-          </ul>
-          {isFetchingNextPage && <p>Loading more users...</p>}
-          <button disabled={isFetchingNextPage || isFetchingUsers || !hasMoreUsers} onClick={() => loadMoreUsers()}>
-            Load more
-          </button>
-        </div>
-      </div> */}
     </>
   );
 };
--- src/ui/navbar/Navbar.tsx
+++ src/ui/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { LoginButton } from 'ui/loginButton/LoginButton';
 import { SearchField } from 'ui/searchField/SearchField';
@@ -8,7 +8,9 @@ export const Navbar = () => {
     <header className="flex justify-center bg-white">
       <div className="w-full px-6 py-8 flex flex-col gap-6 lg:flex-row lg:gap-[108px] lg:max-w-[1224px] 2xl:px-0 ">
         <div className="flex justify-between align-center">
-          <h3 className="font-semibold text-2xl text-almostBlack leading-10">join.tsh.io</h3>
+          <Link to="/">
+            <h3 className="font-semibold text-2xl text-almostBlack leading-10">join.tsh.io</h3>
+          </Link>
           <LoginButton classes="lg:hidden" />
         </div>
         <div className="w-full flex items-center justify-between">
